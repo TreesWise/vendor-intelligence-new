b62776d0ddb79d1aa169a4c405af2ff6fddf49ab
TreesWise <140375595+TreesWise@users.noreply.github.com>
2024-12-11 01:45:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -171,6 +171,6 @@ async def handle_query(userinput: ModelInput) -> Dict:
         return {"response": f"Database '{table_name}' is not supported."}
 
 # Run the FastAPI app (if needed for standalone execution)
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
