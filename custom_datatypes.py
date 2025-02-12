1e30dc3408341058aaace433b4f316b714e892e7
TreesWise <140375595+TreesWise@users.noreply.github.com>
2025-02-12 13:11:49 +0530

    Update custom_datatypes.py

--- custom_datatypes.py
+++ custom_datatypes.py
@@ -6,9 +6,13 @@
 #     Db_Name: str = str
 #     User_Query: str = str
 
-from typing import Optional
+from typing import Optional,List
 from pydantic import BaseModel, Field
 
 class ModelInput(BaseModel):
     db_name: str = Field("vendor intelligence", description="The name of the database")  # Required field
     user_query: Optional[str] = Field(None, description="The query provided by the user")  # Optional field
+    item_id: Optional[List[int]] = []  # Allow multiple item IDs
+    port_id: Optional[List[int]] = []  # Allow multiple port IDs
+    item_name: Optional[List[str]] = []  # Allow multiple item names
+    port_name: Optional[List[str]] = []  # Allow multiple port names
