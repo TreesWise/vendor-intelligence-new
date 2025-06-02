4c05189f317f51082df3dfa09216b64db7ef17ac
TreesWise <140375595+TreesWise@users.noreply.github.com>
2025-06-02 13:56:07 +0530

    Delete startup.sh

--- startup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "🔄 Cleaning .pyc files..."
-find . -type f -name "*.pyc" -delete
-
-echo "🚀 Starting Gunicorn server..."
-exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app --bind 0.0.0.0:8000
